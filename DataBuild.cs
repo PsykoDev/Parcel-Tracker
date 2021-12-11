0dfe21f6668bef38d7a1702d340237cea38de96c
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-12-11 14:31:30 +0100

    Add files via upload

---
+++ DataBuild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using Chronopost.Data;
+using Newtonsoft.Json;
+
+namespace Chronopost.Build
+{
+    public class DataBuild
+    {
+        internal static DataStruct.Root Data(WebResponse response)
+        {
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                if (Chronopost.Debug)
+                    Console.WriteLine(responseFromServer);
+
+                DataStruct.Root myDeserializedClass = JsonConvert.DeserializeObject<DataStruct.Root>(responseFromServer);
+                return myDeserializedClass;
+
+            }
+        }
+    }
+}
+
